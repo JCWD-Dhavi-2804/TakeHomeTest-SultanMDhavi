599ce9f89f030c15c9bb0d892a29fc042d7e5c3e
dhavisiregar <sultanmuhammaddhavi@gmail.com>
2024-08-01 11:36:32 +0700

    another commit

--- src/components/HomePage/Navbar.tsx
+++ src/components/HomePage/Navbar.tsx
@@ -1,18 +1,12 @@
 "use client";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import "./Navbar.css";
 
 const Navbar = () => {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     router.push("/home");
-  };
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
   };
 
   return (
